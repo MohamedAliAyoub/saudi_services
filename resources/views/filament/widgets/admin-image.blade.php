f56091c7ec912de8ead888bc826b1d2bc57b0633
MohamedAliAyoub <mohamedali163163@gmail.com>
2025-02-22 14:13:25 +0200

    customize widgets in dark mode

--- resources/views/filament/widgets/admin-image.blade.php
+++ resources/views/filament/widgets/admin-image.blade.php
@@ -1,7 +1,7 @@
-<div class="filament-info-widget p-4 bg-white shadow rounded-lg">
+<div class="filament-info-widget p-4 bg-white dark:bg-gray-800 shadow rounded-lg">
     <div class="flex items-center gap-3">
-        <img src="{{ env('APP_IMAGE') }}" alt="{{__('message.company_name')}}" class="h-10 w-10 rounded-full">
-        <h1>{{ __('message.company_name') }}</h1>
+        <img src="{{ env('APP_IMAGE') }}" alt="{{ __('message.company_name') }}" class="h-10 w-10 rounded-full">
+        <h1 class="text-gray-900 dark:text-gray-100">{{ __('message.company_name') }}</h1>
     </div>
-    <p>{{ __('message.company_welcome') }} </p>
+    <p class="text-gray-700 dark:text-gray-300">{{ __('message.company_welcome') }}</p>
 </div>
--- resources/views/filament/widgets/client-image.blade.php
+++ resources/views/filament/widgets/client-image.blade.php
@@ -1,7 +1,7 @@
-<div class="filament-info-widget p-4 bg-white shadow rounded-lg">
+<div class="filament-info-widget p-4 bg-white dark:bg-gray-800 shadow rounded-lg">
     <div class="flex items-center gap-3">
-        <img src="{{ auth()->user()->image_url}}" alt="User Avatar" class="h-10 w-10 rounded-full">
-        <h1>{{ __('message.welcome') }}</h1>
+        <img src="{{ auth()->user()->image_url }}" alt="User Avatar" class="h-10 w-10 rounded-full">
+        <h1 class="text-gray-900 dark:text-gray-100">{{ __('message.welcome') }}</h1>
     </div>
-    <p>{{ __('message.welcome') }} {{ __('message.APP_NAME') }}</p>
+    <p class="text-gray-700 dark:text-gray-300">{{ __('message.welcome') }} {{ __('message.APP_NAME') }}</p>
 </div>
