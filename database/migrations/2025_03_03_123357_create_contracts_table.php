f4ac467e16a176c7c45362d6513e3c40542db932
MohamedAliAyoub <mohamedali163163@gmail.com>
2025-03-04 14:44:13 +0200

    add contract in client

---
+++ database/migrations/2025_03_03_123357_create_contracts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained('users');
+            $table->tinyInteger('visits_number')->nullable();
+            $table->date('contract_create_date')->nullable();
+            $table->date('contract_end_date')->nullable();
+            $table->tinyInteger('store_numbers')->nullable();
+            $table->string('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contracts');
+    }
+};
