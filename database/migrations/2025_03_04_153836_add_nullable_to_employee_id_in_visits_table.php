a1ca28cae868b3e56bcd09f449384570c18060d5
MohamedAliAyoub <mohamedali163163@gmail.com>
2025-03-17 01:53:55 +0200

    refactor migration

--- database/migrations/2025_03_04_153836_add_nullable_to_employee_id_in_visits_table.php
+++ database/migrations/2025_03_04_153836_add_nullable_to_employee_id_in_visits_table.php
@@ -4,21 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('visits', function (Blueprint $table) {
-            //drop the foreign key
-            if ($table->hasColumn('employee_id')){
-            $table->dropForeign(['employee_id']);
-            $table->dropColumn('employee_id');
-        }
-        if (!$table->hasColumn('employee_id'))
-            $table->foreignId('employee_id')->nullable()->constrained("users")->onDelete('cascade');
+            if (Schema::hasColumn('visits', 'employee_id')) {
+                $table->dropForeign(['employee_id']);
+                $table->dropColumn('employee_id');
+            }
+            if (!Schema::hasColumn('visits', 'employee_id'))
+                $table->foreignId('employee_id')->nullable()->constrained("users")->onDelete('cascade');
         });
     }
 
